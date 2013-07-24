9704cfa087d5711a2580ff638ef409984941ce72
Justin Wiblin <the_lone_devil@justin-wiblin.tk>
2013-07-24 10:52:55 +0100

    Update for API
    Signed-off-by: Justin Wiblin <the_lone_devil@justin-wiblin.tk>

--- TLDSpells/src/main/java/com/github/thelonedevil/TLDSpells/Spells/Bolt.java
+++ TLDSpells/src/main/java/com/github/thelonedevil/TLDSpells/Spells/Bolt.java
@@ -1,6 +1,6 @@
 package com.github.thelonedevil.TLDSpells.Spells;
 
-import org.spout.api.component.entity.SceneComponent;
+import org.spout.api.component.entity.PhysicsComponent;
 import org.spout.api.entity.Entity;
 import org.spout.api.entity.Player;
 import org.spout.api.geo.World;
@@ -14,10 +14,10 @@ public class Bolt {
 	@SuppressWarnings("unchecked")
 	public static Entity cast(Player p, Class<? extends Substance> projectile) {
 		World world = p.getWorld();
-		Substance item = world.createEntity(p.getScene().getPosition().add(0, 1.6f, 0), projectile).add(projectile);
-		SceneComponent scene = item.getOwner().getScene();
-		// scene.setShape(mass, new SphereShape(0.1f)); // TODO: Correct this
-		scene.impulse(VectorMath.getDirection(p.getScene().getRotation()).multiply(250)).setMovementVelocity(Vector3.FORWARD).force(Vector3.FORWARD);// TODO: Need real parameters
+		Substance item = world.createEntity(p.getPhysics().getPosition().add(0, 1.6f, 0), projectile).add(projectile);
+		PhysicsComponent scene = item.getOwner().getPhysics();
+		scene.setMass(1.0f); // TODO: Correct this
+		scene.impulse(VectorMath.getDirection(p.getPhysics().getRotation()).multiply(250)).setMovementVelocity(Vector3.FORWARD).force(Vector3.FORWARD);// TODO: Need real parameters
 		if (item instanceof Projectile) {
 			((Projectile) item).setShooter(p);
 		}
