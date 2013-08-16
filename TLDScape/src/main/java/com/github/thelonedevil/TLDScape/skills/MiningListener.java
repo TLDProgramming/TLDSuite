47297c5c71bb5309675fccae6660998a0636e01a
Justin Wiblin <the_lone_devil@justin-wiblin.tk>
2013-08-16 12:03:50 +0100

    incremented version to 1.4-SNAPSHOT

--- TLDScape/src/main/java/com/github/thelonedevil/TLDScape/skills/MiningListener.java
+++ TLDScape/src/main/java/com/github/thelonedevil/TLDScape/skills/MiningListener.java
@@ -13,7 +13,7 @@ import com.github.thelonedevil.TLDCommonlib.DataBase;
 import com.github.thelonedevil.TLDCommonlib.Lib;
 import com.github.thelonedevil.TLDScape.TLDScapePlugin;
 
-public class MiningListener implements Listener{
+public class MiningListener implements Listener {
 	private DataBase database = new DataBase();
 	private TLDScapePlugin plugin;
 
@@ -24,53 +24,55 @@ public class MiningListener implements Listener{
 	@EventHandler
 	public void onMine(PlayerInteractBlockEvent event) {
 		Action action = event.getAction();
-		Player p = event.getEntity();
-		String name = p.getName();
-		HashMap<String, Object> skills = null;
-		try {
-			skills = database.getRow(Lib.statement, "ScapeSkills", name, "Player");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (skills != null) {
-			int xp = (Integer) skills.get("Mining");
-			if (xp >= Levels.L1) {
-				// wooden
-			} else if (xp >= Levels.L2) {
-			} else if (xp >= Levels.L3) {
-			} else if (xp >= Levels.L4) {
-			} else if (xp >= Levels.L5) {
-			} else if (xp >= Levels.L6) {
-				// stone
-			} else if (xp >= Levels.L7) {
-			} else if (xp >= Levels.L8) {
-			} else if (xp >= Levels.L9) {
-			} else if (xp >= Levels.L10) {
-			} else if (xp >= Levels.L11) {
-			} else if (xp >= Levels.L12) {
-				// gold
-			} else if (xp >= Levels.L13) {
-			} else if (xp >= Levels.L14) {
-			} else if (xp >= Levels.L15) {
-			} else if (xp >= Levels.L16) {
-			} else if (xp >= Levels.L17) {
-			} else if (xp >= Levels.L18) {
-				// iron
-			} else if (xp >= Levels.L19) {
-			} else if (xp >= Levels.L20) {
-			} else if (xp >= Levels.L21) {
-			} else if (xp >= Levels.L22) {
-			} else if (xp >= Levels.L23) {
-			} else if (xp >= Levels.L24) {
-				// diamond
-			} else if (xp >= Levels.L25) {
-			} else if (xp >= Levels.L26) {
-			} else if (xp >= Levels.L27) {
-			} else if (xp >= Levels.L28) {
-			} else if (xp >= Levels.L29) {
-			} else if (xp >= Levels.L30) {
-				// faster mining
+		if (action == Action.RIGHT_CLICK) {
+			Player p = event.getEntity();
+			String name = p.getName();
+			HashMap<String, Object> skills = null;
+			try {
+				skills = database.getRow(Lib.statement, "ScapeSkills", name, "Player");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (skills != null) {
+				int xp = (Integer) skills.get("Mining");
+				if (xp >= Levels.L1) {
+					// wooden
+				} else if (xp >= Levels.L2) {
+				} else if (xp >= Levels.L3) {
+				} else if (xp >= Levels.L4) {
+				} else if (xp >= Levels.L5) {
+				} else if (xp >= Levels.L6) {
+					// stone
+				} else if (xp >= Levels.L7) {
+				} else if (xp >= Levels.L8) {
+				} else if (xp >= Levels.L9) {
+				} else if (xp >= Levels.L10) {
+				} else if (xp >= Levels.L11) {
+				} else if (xp >= Levels.L12) {
+					// gold
+				} else if (xp >= Levels.L13) {
+				} else if (xp >= Levels.L14) {
+				} else if (xp >= Levels.L15) {
+				} else if (xp >= Levels.L16) {
+				} else if (xp >= Levels.L17) {
+				} else if (xp >= Levels.L18) {
+					// iron
+				} else if (xp >= Levels.L19) {
+				} else if (xp >= Levels.L20) {
+				} else if (xp >= Levels.L21) {
+				} else if (xp >= Levels.L22) {
+				} else if (xp >= Levels.L23) {
+				} else if (xp >= Levels.L24) {
+					// diamond
+				} else if (xp >= Levels.L25) {
+				} else if (xp >= Levels.L26) {
+				} else if (xp >= Levels.L27) {
+				} else if (xp >= Levels.L28) {
+				} else if (xp >= Levels.L29) {
+				} else if (xp >= Levels.L30) {
+					// faster mining
+				}
 			}
 		}
 	}
